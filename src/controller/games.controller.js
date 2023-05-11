405db5abbffb7cb40894aec12e83f3449e9329e3
Kaiena <kaienafmartins@gmail.com>
2023-05-11 00:53:22 -0300

    fix name validation

--- src/controller/games.controller.js
+++ src/controller/games.controller.js
@@ -14,25 +14,17 @@ export async function postGames(req, res) {
   const { name, image, stockTotal, pricePerDay } = req.body;
 
   try {
-    !name ||
-      name.trim().length === 0 ||
-      !stockTotal ||
-      !pricePerDay ||
-      stockTotal <= 0 ||
-      pricePerDay <= 0;
-    return res.status(400).json({ error: "Dados inválidos!" });
+    if (!name || typeof name !== 'string' || name.trim().length === 0 || !stockTotal || !pricePerDay || stockTotal <= 0 || pricePerDay <= 0) {
+      return res.status(400).json({ error: "Dados inválidos!" });
+    }
 
-    const gameExists = await db.query(
-      `SELECT name FROM games WHERE name='${name}'`
-    );
+    const gameExists = await db.query(`SELECT name FROM games WHERE name='${name}'`);
 
     if (gameExists.rows.length > 0) {
       return res.status(409).send("Esse jogo já existe!");
     }
 
-    const game = await db.query(
-      `INSERT INTO games (name , image, "stockTotal", "pricePerDay") VALUES ('${name}', '${image}', ${stockTotal}, ${pricePerDay});`
-    );
+    const game = await db.query(`INSERT INTO games (name , image, "stockTotal", "pricePerDay") VALUES ('${name}', '${image}', ${stockTotal}, ${pricePerDay});`);
 
     if (game.rowCount > 0) {
       console.table(game.rows);
