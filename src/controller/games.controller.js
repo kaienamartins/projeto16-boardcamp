babba4d1788a40974a8df0d2857d583fa307dfd9
Kaiena <kaienafmartins@gmail.com>
2023-05-10 21:08:59 -0300

    fix game controller

--- src/controller/games.controller.js
+++ src/controller/games.controller.js
@@ -9,18 +9,33 @@ export async function getGames(req, res) {
   }
 }
 
+export async function postGames(req, res) {
+  const { name, image, stockTotal, pricePerDay } = req.body;
 
-export async function postGames(req, res){
-  try{  
-    const {name, stockTotal, pricePerDay } = req.body;
+  try {
     const games = await db.query("SELECT * FROM games WHERE name = $1", [name]);
-    if(games.rows.length !== 0){
-      res.sendStatus(409);
+    if (games.rows.length > 0) {
+      res.status(409).send("Esse jogo já existe!");
     }
-    if(name === "" || stockTotal <= 0 || pricePerDay <= 0){
-      res.sendStatus(400);
+
+    if (
+      !name ||
+      !stockTotal ||
+      !pricePerDay ||
+      stockTotal <= 0 ||
+      pricePerDay <= 0
+    ) {
+      res.status(400).send("Dados inválidos!");
     }
-  }catch(error){
+
+    const game = await db.query(
+      'INSERT INTO games (name, image, "stockTotal", "pricePerDay") VALUES ($1, $2, $3, $4)',
+      [name, image, stockTotal, pricePerDay]
+    );
+    console.table(game.rows);
+
     res.sendStatus(201);
+  } catch (error) {
+    res.status(500).send(error);
   }
 }
