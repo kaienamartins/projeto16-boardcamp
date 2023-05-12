01e1b54e408c80152d77f9e9c445b2f7a8a2b829
Kaiena <kaienafmartins@gmail.com>
2023-05-12 19:49:43 -0300

    change birthday on schema

--- src/schemas/customers.schema.js
+++ src/schemas/customers.schema.js
@@ -4,5 +4,5 @@ export const customersSchema = joi.object({
   name: joi.string().required(),
   phone: joi.string().pattern(/^[0-9]{10,11}$/).required(),
   cpf: joi.string().pattern(/^[0-9]{11}$/).required(),
-  birthday: joi.date().required(),
+  birthday: joi.date().iso().required()
 });
