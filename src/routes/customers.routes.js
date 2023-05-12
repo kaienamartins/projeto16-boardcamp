fac7e3265f4c876facbab705a267c9999b29b2eb
Kaiena <kaienafmartins@gmail.com>
2023-05-12 19:36:25 -0300

    fix customerbyid

--- src/routes/customers.routes.js
+++ src/routes/customers.routes.js
@@ -11,7 +11,7 @@ import { customersSchema } from "../schemas/customers.schema.js";
 const customersRouter = Router();
 
 customersRouter.get("/customers", getCustomers);
-customersRouter.get("/customers:id", getCustomersById);
+customersRouter.get("/customers/:id", getCustomersById);
 customersRouter.post(
   "/customers",
   validateSchema(customersSchema),
