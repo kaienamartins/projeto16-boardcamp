f32f8c0b94bfc28e1b1164f292b6084f0c033012
Kaiena <kaienafmartins@gmail.com>
2023-05-14 19:27:46 -0300

    fix delay again

--- src/controller/rentals.controller.js
+++ src/controller/rentals.controller.js
@@ -140,7 +140,8 @@ export async function postReturns(req, res) {
       rentedDurationInMilliseconds / (1000 * 60 * 60 * 24)
     );
     const delayInDays = Math.max(0, rentedDurationInDays - daysRented);
-    const delayFeePerDay = rentalExists.rows[0].originalPrice * 0.1;
+    const delayFeePerDayFraction = 0.1;
+    const delayFeePerDay = pricePerDay * delayFeePerDayFraction;
     const delayFee = delayFeePerDay * delayInDays;
 
     await db.query(
